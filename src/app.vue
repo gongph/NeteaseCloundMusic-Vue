a9ec9532e911be03acd272ed7aef6789e561fb5b
gongph <gongph@foxmail.com>
2018-08-20 17:07:22 +0800

    init file

---
+++ src/app.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="main">{{ msg }}</div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        msg: 'hello, vue.js!'
+      }
+    }
+  }
+</script>
+
+<style lang="less">
+  .main {
+    color: red;
+  }
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
+module.exports = {
+  entry: {
+    app: './src/app.js'
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.less$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          'less-loader'
+        ]
+      },
+      {
+        test: /\.vue$/,
+        use: [
+          'vue-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+      filename: 'index.html'
+    }),
+    new VueLoaderPlugin()
+  ]
+}
